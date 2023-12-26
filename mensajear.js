882594ba3c793899e10191f7e6b50f7faaa6be66
Frank Cairampoma <ingfralch@gmail.com>
2023-12-26 17:44:03 -0500

    funcionando en ubuntu

--- mensajear.js
+++ mensajear.js
@@ -1,27 +1,22 @@
 const qrcode = require('qrcode-terminal');
 
 const { Client, LocalAuth } = require('whatsapp-web.js');
-
-const client = new Client({
-    authStrategy: new LocalAuth()
-});
- 
-
-
-// (async () => {
-//     client.on('qr', qr => {
-//         qrcode.generate(qr, { small: true });
-//     });
-
-//     client.on('ready', () => {
-//         console.log('Client is ready!');
-//     });
-
-//     client.initialize();
-
-// })();
+const puppeteer = require('puppeteer');
 
 const startClient = async () => {
+    const browser = await puppeteer.launch({
+        headless: 'new',
+        executablePath: '/usr/bin/chromium-browser', // Puedes especificar la ruta del ejecutable de Chromium aquí
+        args: ['--no-sandbox'], // Agrega esta línea para deshabilitar el sandbox
+    });
+
+    const client = new Client({
+        authStrategy: new LocalAuth(),
+        puppeteer: {
+            browserWSEndpoint: (await browser.wsEndpoint()), // Conectar Puppeteer con WhatsApp Web.js
+        },
+    });
+
     client.on('qr', qr => {
         qrcode.generate(qr, { small: true });
     });
@@ -35,15 +30,9 @@ const startClient = async () => {
     return client;
 }
 
-function sendTextMessage(number, message) {
-    // client.sendMessage(number, message)
-    // .then(response => {
-    //     console.log(response);
-    // })
-}
+
 
 module.exports = {
-    startClient,
-    sendTextMessage
+    startClient
 }
  
