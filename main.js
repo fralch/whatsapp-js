b408f551bf7b31ae4347de050e2631cdf0650205
Frank Cairampoma <ingfralch@gmail.com>
2023-12-28 15:55:55 -0500

    funciono ojala este si

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 
+const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const puppeteer = require('puppeteer');
+
 
 app.use(cors()); // cors se utiliza para que el servidor pueda recibir peticiones de otros servidores si no se utiliza no se podra recibir peticiones de otros servidores
 app.use(express.json()); // express.json() se utiliza para que el servidor pueda recibir peticiones en formato json
@@ -11,11 +15,50 @@ app.use(bodyParser.urlencoded({ extended: false })); // bodyParser.urlencoded() 
 // el formato urlencoded es el que se utiliza en los formularios de html
 app.use(express.json({ limit: '50mb' }));
 
+// iniciar servicio de whatsapp web js 
+
+const browser = async function () {
+    return browser = await puppeteer.launch({
+        headless: 'new',
+        executablePath: '/usr/bin/chromium-browser', // Puedes especificar la ruta del ejecutable de Chromium aquí
+        args: ['--no-sandbox'], // Agrega esta línea para deshabilitar el sandbox
+    });
+
+}
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: true,
+        args: ["--no-sandbox"]
+    }
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
 
 const port = 3000;
 
-const rutas = require('./routes/index');
-app.use('/api', rutas);
+
+app.post('/api/whatsapp', async (req, res) => {
+    const { body } = req;
+    const { mensaje, numero } = body;
+
+
+    const targetNumber = `51${numero}@c.us`;
+    const message = `${mensaje}`;
+
+    await client.sendMessage(targetNumber, message);
+    console.log('Mensaje enviado correctamente');
+    res.send('Mensaje enviado correctamente');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
