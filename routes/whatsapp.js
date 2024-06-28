37471c7b9be68c03131d25cd6a53c5646bee6b8c
FrankCairampoma <ingfralch@gmail.com>
2024-06-28 09:06:32 -0500

    chore: Remove unnecessary files and folders

--- routes/whatsapp.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { enviarMensaje } = require('../controllers/whatsappCrtl');
-
-router.post('/', enviarMensaje);
-
-module.exports = router;
